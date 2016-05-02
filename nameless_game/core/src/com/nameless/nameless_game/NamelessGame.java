1e7db6c130ac11738c51f62b5e5ae25eb5f7b66a
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-02 14:23:58 +0200

    Testing drawing cube to screen.

--- nameless_game/core/src/com/nameless/nameless_game/NamelessGame.java
+++ nameless_game/core/src/com/nameless/nameless_game/NamelessGame.java
@@ -2,26 +2,48 @@ package com.nameless.nameless_game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
+import com.sun.glass.ui.Pixels;
+import com.sun.glass.ui.Pixels.Format;
 
 public class NamelessGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	ShapeRenderer shapeRenderer;
+	Rectangle rectangle;
+	
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
+		rectangle = new Rectangle(100,100,100,100);
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 600);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render () {		
+		Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		camera.update();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(1, 0, 0, 1);
+		
+		shapeRenderer.translate(1, 0, 0);
+		
+		shapeRenderer.rect(0, 0, rectangle.width, rectangle.height);
+		
+		shapeRenderer.end();
+		
 	}
 }
