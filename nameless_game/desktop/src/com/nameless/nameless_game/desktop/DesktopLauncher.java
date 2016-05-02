8c5a3e3f7408c81a51193d143c3f414ff0b7fbbd
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-02 12:27:01 +0200

    Setup LibGDX project.

---
+++ nameless_game/desktop/src/com/nameless/nameless_game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nameless.nameless_game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nameless.nameless_game.NamelessGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NamelessGame(), config);
+	}
+}
