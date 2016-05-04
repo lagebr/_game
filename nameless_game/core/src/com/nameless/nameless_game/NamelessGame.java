fd118453ca6471e1082b13e6896b3381d65c73a4
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-04 10:12:27 +0200

    Added renderer, world and entities fields, initalized them in create method.

--- nameless_game/core/src/com/nameless/nameless_game/NamelessGame.java
+++ nameless_game/core/src/com/nameless/nameless_game/NamelessGame.java
@@ -1,6 +1,15 @@
 package com.nameless.nameless_game;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.nameless.nameless_game.model.Entity;
+import com.nameless.nameless_game.render.Renderer;
+import com.nameless.nameless_game.render.ScreenRenderer;
 
 /**
  * NamelessGame is the main controller for the game. Handles input and updates.
@@ -9,14 +18,24 @@ import com.badlogic.gdx.ApplicationAdapter;
  * @version 2016-05-04
  */
 public class NamelessGame extends ApplicationAdapter {
+
+	Renderer renderer;
+	World world;
 	
+	ArrayList<Entity> entities;
+
 	@Override
 	public void create() {
-
+		renderer = new ScreenRenderer(800, 600);
+		world = new World(new Vector2(0, 0), true);
+		
+		Texture texture = new Texture(Gdx.files.internal("image.png"));
+		Entity entity = new Entity(100, 100, 100, 100, texture, world);
+		entities.add(entity);
 	}
 
 	@Override
 	public void render() {
-
+		
 	}
 }
