33b8133be54088e7bab833d7ef65b91d7ef3a5ac
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-04 11:27:50 +0200

    Created InputAction enum.

---
+++ nameless_game/core/src/com/nameless/nameless_game/InputAction.java
@@ -0,0 +1,11 @@
+package com.nameless.nameless_game;
+
+/**
+ * InputAction is an action which can be triggered by an input event.
+ * 
+ * @author Isaac Arvestad, Henrik Lagebrand
+ * @version 2016-05-04
+ */
+public enum InputAction {
+	UP_PRESSED, UP_RELEASED, DOWN_PRESSED, DOWN_RELEASED, LEFT_PRESSED, LEFT_RELEASED, RIGHT_PRESSED, RIGHT_RELEASED;
+}
