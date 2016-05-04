940e284c72eb88d81c96fccbd37fc57cb2314cbe
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-04 09:00:38 +0200

    Created an entity class and added two fields, body and texture.

---
+++ nameless_game/core/src/com/nameless/nameless_game/model/Entity.java
@@ -0,0 +1,15 @@
+package com.nameless.nameless_game.model;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * Entity describes a base entity which has a physics body and a texture for rendering.
+ * 
+ * @author Isaac Arvestad, Henrik Lagebrand
+ * @version 2016-05-04
+ */
+public class Entity {
+	private Body body;
+	private Texture texture;
+}
