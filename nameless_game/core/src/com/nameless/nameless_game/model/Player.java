cf6385c97cb5d3bb63bc3171b4364348bb722fd0
lagebr <lagebr@kth.se>
2016-05-05 10:38:33 +0200

    Added a dynamic physics body.

--- nameless_game/core/src/com/nameless/nameless_game/model/Player.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/Player.java
@@ -1,6 +1,11 @@
 package com.nameless.nameless_game.model;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
@@ -12,10 +17,65 @@ import com.badlogic.gdx.physics.box2d.World;
  */
 public class Player extends Entity {
 
+	/**
+	 * The player entity model creates a player entity in the world, with a
+	 * dynamic physics body.
+	 * 
+	 * @param x
+	 *            The x center of the body
+	 * @param y
+	 *            The y center of the body
+	 * @param width
+	 *            The width of the body
+	 * @param height
+	 *            The height of the body
+	 * @param texture
+	 *            The texture of the entity
+	 * @param world
+	 *            The world to add the body to
+	 * @return the physics body
+	 */
 	public Player(float x, float y, float width, float height, Texture texture, World world) {
-		super(x, y, width, height, texture, world);
-		
-	}
-	
+		super(texture); // calls alternative super-constructor from Entity
 
+		body = createDynamicBody(x, y, width, height, world);
+
+	}
+
+	/**
+	 * createBody creates a rectangular, static physics body, adds it to the
+	 * physics world and returns it.
+	 * 
+	 * @param x
+	 *            the x center of the body
+	 * @param y
+	 *            the y center of the body
+	 * @param width
+	 *            the width of the body
+	 * @param height
+	 *            the height of the body
+	 * @param world
+	 *            the world to add the body to
+	 * @return the physics body
+	 */
+	private static Body createDynamicBody(float x, float y, float width, float height, World world) {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(x, y);
+
+		Body physicsBody = world.createBody(bodyDef);
+
+		CircleShape circle = new CircleShape();
+		circle.setRadius(60);
+
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f; // hardy
+		fixtureDef.friction = 0.4f; // frit
+		fixtureDef.restitution = 0.6f; // bounce
+
+		circle.dispose(); // openGL
+
+		return physicsBody;
+	}
 }
