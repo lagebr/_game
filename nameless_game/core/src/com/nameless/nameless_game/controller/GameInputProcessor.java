f6821d743b0999f9ee0dd6c6b956e82877ca474c
lagebr <lagebr@kth.se>
2016-05-05 09:52:54 +0200

    added documentation for GameINputProcessor

--- nameless_game/core/src/com/nameless/nameless_game/controller/GameInputProcessor.java
+++ nameless_game/core/src/com/nameless/nameless_game/controller/GameInputProcessor.java
@@ -15,32 +15,39 @@ import com.badlogic.gdx.InputAdapter;
  * @version 2016-05-04
  */
 public class GameInputProcessor extends InputAdapter {
-
+	// The mapping of keys (ie keycodes) to specific InputActions
 	private Map<Integer, InputAction> actionLookUp = new HashMap<Integer, InputAction>();
-
+	// The queue for the processed InputActions (ie InputEvents)
 	private List<InputEvent> actionQueue = new LinkedList<InputEvent>();
 
 	public GameInputProcessor() {
+		// Moving LEFT
 		actionLookUp.put(Input.Keys.A, InputAction.LEFT);
 		actionLookUp.put(Input.Keys.LEFT, InputAction.LEFT);
+		// Moving RIGHT
 		actionLookUp.put(Input.Keys.D, InputAction.RIGHT);
 		actionLookUp.put(Input.Keys.RIGHT, InputAction.RIGHT);
-		
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public boolean keyDown(int keycode) {
 		InputAction action = actionLookUp.get(keycode);
-
+		// If null the LookUp found no corresponding InputAction
 		if (action != null) {
 			InputEvent event = new InputEvent(action, true);
-			
+			// Otherwise we init and add the InputEvent to the queue
 			actionQueue.add(event);
 		}
-		
+	
 		return true;
 	}
-
+	
+	/**
+	 * 
+	 */
 	@Override
 	public boolean keyUp(int keycode) {
 		InputAction action = actionLookUp.get(keycode);
@@ -53,7 +60,11 @@ public class GameInputProcessor extends InputAdapter {
 		
 		return true;
 	}
-
+	
+	/**
+	 * Getter for the ActionQueue.
+	 * @return The ActionQueue
+	 */
 	public List<InputEvent> getActionQueue() {
 		return actionQueue;
 	}
