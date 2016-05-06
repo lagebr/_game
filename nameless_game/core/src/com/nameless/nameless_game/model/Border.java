a8464af16d3ed46a34c7dfc86d016695f0b30326
lagebr <lagebr@kth.se>
2016-05-06 20:03:01 +0200

    Added Border entity.

---
+++ nameless_game/core/src/com/nameless/nameless_game/model/Border.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nameless.nameless_game.model;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * @author lagebr
+ *
+ */
+public class Border extends Entity {
+
+	public Border(float x, float y, float width, float height, Texture texture, World world) {
+		super(x, y, width, height, texture, world);
+		// TODO Auto-generated constructor stub
+	}
+
+}
