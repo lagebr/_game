5b396a7bdd7c17e56cf8846121b81a6cacbc7eff
lagebr <lagebr@kth.se>
2016-05-06 20:07:04 +0200

    Added empty constructor call.

--- nameless_game/core/src/com/nameless/nameless_game/model/Border.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/Border.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.physics.box2d.World;
 public class Border extends Entity {
 
 	public Border(float x, float y, float width, float height, Texture texture, World world) {
-		super(x, y, width, height, texture, world);
+		super();
 		// TODO Auto-generated constructor stub
 	}
 
