0dc49187d87f315b6a202533da32a6e3df8fb5be
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-06 19:58:56 +0200

    Moved responsibility of converting pixels to meters to within entity and player constructor.

--- nameless_game/core/src/com/nameless/nameless_game/model/Player.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/Player.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.nameless.nameless_game.render.ScreenRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
@@ -17,19 +18,17 @@ import com.badlogic.gdx.physics.box2d.World;
  *
  */
 public class Player extends Entity {
-	
+
 	/**
 	 * The player entity model creates a player entity in the world, with a
 	 * dynamic physics body.
 	 * 
 	 * @param x
-	 *            The x center of the body
+	 *            The x center of the body in number of pixels.
 	 * @param y
-	 *            The y center of the body
-	 * @param width
-	 *            The width of the body
-	 * @param height
-	 *            The height of the body
+	 *            The y center of the body in number of pixels.
+	 * @param radius
+	 *            The radius of the body in number of pixels.
 	 * @param texture
 	 *            The texture of the entity
 	 * @param world
@@ -39,7 +38,8 @@ public class Player extends Entity {
 	public Player(float x, float y, float radius, Texture texture, World world) {
 		super(texture); // calls alternative super-constructor from Entity
 
-		body = createDynamicBody(x, y, radius, world);
+		body = createDynamicBody(ScreenRenderer.pixelToMeter(x), ScreenRenderer.pixelToMeter(y),
+				ScreenRenderer.pixelToMeter(radius), world);
 	}
 
 	/**
@@ -47,11 +47,11 @@ public class Player extends Entity {
 	 * physics world and returns it.
 	 * 
 	 * @param x
-	 *            the x center of the body
+	 *            The x center of the body in physics meters.
 	 * @param y
-	 *            the y center of the body
+	 *            The y center of the body in physics meters.
 	 * @param radius
-	 *            The radius of the circle
+	 *            The radius of the circle in physics meters.
 	 * @param world
 	 *            the world to add the body to
 	 * @return the physics body
@@ -71,10 +71,12 @@ public class Player extends Entity {
 		fixtureDef.density = 0.5f; // hardy
 		fixtureDef.friction = 0.4f; // frit
 		fixtureDef.restitution = 0.6f; // bounce
-        fixtureDef.filter.categoryBits = Entity.PLAYER_ENTITY; // this what I am
-        fixtureDef.filter.maskBits = Entity.NPC_ENTITY; // this is what I collide with
-        physicsBody.createFixture(fixtureDef); // if activate makes Player run out of screen
-        
+		fixtureDef.filter.categoryBits = Entity.PLAYER_ENTITY; // this what I am
+		fixtureDef.filter.maskBits = Entity.NPC_ENTITY; // this is what I
+														// collide with
+		physicsBody.createFixture(fixtureDef); // if activate makes Player run
+												// out of screen
+
 		circle.dispose(); // openGL
 
 		return physicsBody;
