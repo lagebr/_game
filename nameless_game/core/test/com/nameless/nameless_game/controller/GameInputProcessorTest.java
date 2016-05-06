234d28969098915507b9566f33f8da572c9d78e9
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-06 16:14:43 +0200

    Created test class for GameInputProcessor.

---
+++ nameless_game/core/test/com/nameless/nameless_game/controller/GameInputProcessorTest.java
@@ -0,0 +1,19 @@
+package com.nameless.nameless_game.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameInputProcessorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
