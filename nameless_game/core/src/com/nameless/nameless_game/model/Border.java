d0f0731baa11e8e2ce6491c8eb21ec4713cb3b82
lagebr <lagebr@kth.se>
2016-05-08 17:05:44 +0200

    Removed BoxBorder().

--- nameless_game/core/src/com/nameless/nameless_game/model/Border.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/Border.java
@@ -6,10 +6,8 @@ package com.nameless.nameless_game.model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.nameless.nameless_game.render.ScreenRenderer;
 
