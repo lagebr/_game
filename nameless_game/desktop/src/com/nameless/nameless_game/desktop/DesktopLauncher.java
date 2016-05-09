b419842b20dd5718ca335ece501e813037beb766
lagebr <lagebr@kth.se>
2016-05-09 10:55:33 +0200

    Changed screen resolution.

--- nameless_game/desktop/src/com/nameless/nameless_game/desktop/DesktopLauncher.java
+++ nameless_game/desktop/src/com/nameless/nameless_game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.nameless.nameless_game.controller.NamelessGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 600;
+		config.width = 1024;
+		config.height = 768;
 		config.foregroundFPS = 60;
 		new LwjglApplication(new NamelessGame(), config);
 	}
