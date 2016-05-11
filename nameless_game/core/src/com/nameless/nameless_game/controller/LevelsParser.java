c42ef6730d3e7d0fac4f20634ba41ddcd212e656
lagebr <lagebr@kth.se>
2016-05-11 15:29:12 +0200

    Added new class.

---
+++ nameless_game/core/src/com/nameless/nameless_game/controller/LevelsParser.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.nameless.nameless_game.controller;
+
+/**
+ * Parses the stored levels from their JSON format to turn them into Java Objects.
+ * 
+ * @author Isaac Arvestad, Henrik Lagebrand
+ * @version 2016-05-11
+ *
+ */
+public class LevelsParser {
+
+	/**
+	 * 
+	 */
+	public LevelsParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
