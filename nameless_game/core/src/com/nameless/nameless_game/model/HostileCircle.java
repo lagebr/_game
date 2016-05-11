c9bbd587943dba19318599bffadb8f2accd03f78
lagebr <lagebr@kth.se>
2016-05-11 17:42:50 +0200

    Tinkerng with parameters.

--- nameless_game/core/src/com/nameless/nameless_game/model/HostileCircle.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/HostileCircle.java
@@ -3,39 +3,50 @@
  */
 package com.nameless.nameless_game.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.World;
 import com.nameless.nameless_game.render.ScreenRenderer;
 
 /**
- * A type of Hostile with a sine-like move pattern.
+ * Spins and if Player is in it's path after a delay it'll launch after Player.
+ * TODO
  * 
  * @author Henrik Lagebrand, Isaac Arvestad
  * @version 2016-05-09
  *
  */
 public class HostileCircle extends Hostile {
-	private double progression = 0f;
-
+	float screenWidth = Gdx.graphics.getWidth();
+	float screenHeight = Gdx.graphics.getHeight();
+	
 	public HostileCircle(float x, float y, float radius, Texture texture, World world) {
 		super(texture);
 		body = createDynamicBody(ScreenRenderer.pixelToMeter(x), ScreenRenderer.pixelToMeter(y),
 				ScreenRenderer.pixelToMeter(radius), world);
-		body.setFixedRotation(false);
-
+		body.setFixedRotation(false); // Needs to spin
 	}
 
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
-		// body.setLinearVelocity(1, 1);
-		progression += deltaTime;
-
-		float forceY = (float) (Math.sin((progression))) / 10;
-		float forceX = (float) (Math.cos((progression))) / 10;
-		body.applyLinearImpulse(new Vector2(forceX, 0), body.getLocalCenter(), true);
-		if (progression > 360)
-			progression = 0;
+		body.setAngularVelocity(1);
+		
+		float x1 = body.getPosition().x;
+		float y1 = body.getPosition().y;
+		// TODO implement cleaner Radius information
+		float x2 = screenWidth - x1;
+		float y2 = 60 * MathUtils.sin(body.getAngle());
+		
+		float x1_corner = screenWidth - screenWidth/8;
+		float y1_corner = screenHeight;
+		float x2_corner = screenWidth;
+		float y2_corner = screenHeight - screenHeight/8;
+		
+		if (body.getAngle() % 2*MathUtils.PI > 2*MathUtils.PI/4) {
+			body.applyForceToCenter(10, 10, true);
+			}
+		
 	}
 }
