59b9b6b02ed7e0a0324da02eb5f56b78ae256f51
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-11 13:32:37 +0200

    Added a constructor which just calls the Hostile constructor.

--- nameless_game/core/src/com/nameless/nameless_game/model/HostileWithTarget.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/HostileWithTarget.java
@@ -1,5 +1,8 @@
 package com.nameless.nameless_game.model;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
  * HostileWithTarget describes a hostile which bases it's behavior on a
  * different Entity which is its' target. Class is abstract and should be
@@ -10,6 +13,10 @@ package com.nameless.nameless_game.model;
  */
 public abstract class HostileWithTarget extends Hostile {
 	protected Entity target;
+	
+	public HostileWithTarget(float x, float y, float width, float height, Texture texture, World world) {
+		super(x, y, width, height, texture, world);
+	}
 	
 	public Entity getTarget() {
 		return target;
