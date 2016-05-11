f0cf573213785cfd188b73ddd26690b124bd5256
lagebr <lagebr@kth.se>
2016-05-11 15:20:04 +0200

    Fixed body to match sprite and played around with the movement pattern.

--- nameless_game/core/src/com/nameless/nameless_game/model/HostileCircle.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/HostileCircle.java
@@ -6,6 +6,7 @@ package com.nameless.nameless_game.model;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.nameless.nameless_game.render.ScreenRenderer;
 
 /**
  * A type of Hostile with a sine-like move pattern.
@@ -15,20 +16,24 @@ import com.badlogic.gdx.physics.box2d.World;
  *
  */
 public class HostileCircle extends Hostile {
-	private double progress = 0f;
+	private double progression = 0f;
 
-	public HostileCircle(float x, float y, float width, float height, Texture texture, World world) {
-		super(x, y, width, height, texture, world);
-
+	public HostileCircle(float x, float y, float radius, Texture texture, World world) {
+		super(texture);
+		body = createDynamicBody(ScreenRenderer.pixelToMeter(x), ScreenRenderer.pixelToMeter(y),
+				ScreenRenderer.pixelToMeter(radius), world);
+		this.setBody(body);
+		
 	}
 
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
-		progress += 15.5 * deltaTime;
-
-		float forceY = (float) Math.sin((progress));
-		float forceX = (float) Math.cos((progress));
-		body.applyLinearImpulse(new Vector2(forceX / 100000, forceY / 100000), body.getLocalCenter(), true);
+		progression += 5.5 * deltaTime;
+		
+		float forceY = (float) (progression * Math.sin((progression)));
+		float forceX = (float) (progression * Math.cos((progression)));
+		body.applyLinearImpulse(new Vector2(forceX/1000, forceY/1000), body.getLocalCenter(), true);
+		if (progression > 360) progression = 0;
 	}
 }
