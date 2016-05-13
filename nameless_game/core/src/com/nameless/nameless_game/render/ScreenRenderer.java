86002fc51f8939f5c7c4fcc9446f9a6f1872a882
lagebr <lagebr@kth.se>
2016-05-13 16:54:39 +0200

    Switching shader.

--- nameless_game/core/src/com/nameless/nameless_game/render/ScreenRenderer.java
+++ nameless_game/core/src/com/nameless/nameless_game/render/ScreenRenderer.java
@@ -31,31 +31,9 @@ public class ScreenRenderer extends Renderer {
 	private FPSLogger logger = new FPSLogger();
 	
 	private ShaderProgram shader;
-	
-	String vertexShader = "attribute vec4 a_position;    \n" + 
-            "attribute vec4 a_color;\n" +
-            "attribute vec2 a_texCoord0;\n" + 
-            "uniform mat4 u_worldView;\n" + 
-            "varying vec4 v_color;" + 
-            "varying vec2 v_texCoords;" + 
-            "void main()                  \n" + 
-            "{                            \n" + 
-            "   v_color = vec4(1, 1, 1, 1); \n" + 
-            "   v_texCoords = a_texCoord0; \n" + 
-            "   gl_Position =  u_worldView * a_position;  \n"      + 
-            "}                            \n";
-	
-	String fragmentShader = "#ifdef GL_ES\n" +
-            "precision mediump float;\n" + 
-            "#endif\n" + 
-            "varying vec4 v_color;\n" + 
-            "varying vec2 v_texCoords;\n" + 
-            "uniform sampler2D u_texture;\n" + 
-            "void main()                                  \n" + 
-            "{                                            \n" + 
-            "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n" +
-            "}";
-
+    String vertexShader;
+    String fragmentShader;
+    
 	/**
 	 * Draws all entities on screen using an perspective camera.
 	 * 
@@ -75,7 +53,9 @@ public class ScreenRenderer extends Renderer {
 		camera.lookAt(width / 2, height / 2, 0);
 		camera.far = 100000;
 		
-		shader = new ShaderProgram(vertexShader, fragmentShader);
+        vertexShader = Gdx.files.internal("vertex.glsl").readString();
+        fragmentShader = Gdx.files.internal("fragment.glsl").readString();
+        shader = new ShaderProgram(vertexShader,fragmentShader);
 	}
 
 	/**
