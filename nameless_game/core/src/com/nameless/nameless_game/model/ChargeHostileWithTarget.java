1d396bdf721a32b8ee0664c2f83e48e1f0c2f47f
lagebr <lagebr@kth.se>
2016-05-13 12:23:20 +0200

    First draft of charging hostile.

--- nameless_game/core/src/com/nameless/nameless_game/model/ChargeHostileWithTarget.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/ChargeHostileWithTarget.java
@@ -3,10 +3,10 @@
  */
 package com.nameless.nameless_game.model;
 
-import java.util.Random;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.nameless.nameless_game.render.ScreenRenderer;
 
@@ -24,9 +24,9 @@ public class ChargeHostileWithTarget extends HostileWithTarget {
 
 	boolean isSleeping = false;
 
-	Random random = new Random();
 	private float timeSlept = 0;
-	private float napTime = 1;
+	private float napTime = 5f;
+	private float angVelocity = .5f;
 
 	public ChargeHostileWithTarget(float x, float y, float radius, Texture texture, World world, Entity target) {
 		super(texture);
@@ -39,32 +39,35 @@ public class ChargeHostileWithTarget extends HostileWithTarget {
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
+		body.setAngularVelocity(angVelocity);
 		if (!isSleeping) {
-			body.setAngularVelocity(1);
-
-			float v1 = 12;
-			float v2 = 12;
-
-			double dx = body.getPosition().x - target.getBody().getPosition().x;
-			double dy = body.getPosition().y - target.getBody().getPosition().y;
+			double dx = target.getBody().getPosition().x - body.getPosition().x;
+			double dy = target.getBody().getPosition().y - body.getPosition().y;
 			double v = Math.atan(dy / dx);
+			if (Math.abs(v - (float)(body.getAngle() % 2*MathUtils.PI)) < 4 * MathUtils.PI/180) {
+				// When target is in sight delay then charge
+				isSleeping = true;
+				angVelocity = 0;
+				}
 
-			if (Math.abs(v - (double) body.getAngle()) < 5 * Math.PI / 180) {
-
-			}
 		} else {
 			timeSlept += deltaTime;
 			if (timeSlept > napTime) {
 				charge();
-				timeSlept = 0;
+				// After charge return to start state
 				isSleeping = false;
+				angVelocity = 1;
+				timeSlept = 0;
 			}
 		}
 
 	}
-
+	
+	/** Charges hostile with impulse in current direction. */
 	private void charge() {
-		
-		
+		float c = 10;
+		float xImpulse = c * MathUtils.cos(body.getAngle());
+		float yImpulse = c * MathUtils.sin(body.getAngle());
+		body.applyLinearImpulse(new Vector2(xImpulse, yImpulse), body.getWorldCenter(), true);
 	}
 }
