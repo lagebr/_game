2d5c94afb6b7329a4f43ce701ec5a502c4c7724b
lagebr <lagebr@kth.se>
2016-05-13 22:40:43 +0200

    Added methods for keyTypes display.

--- nameless_game/core/src/com/nameless/nameless_game/model/Level.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/Level.java
@@ -3,6 +3,7 @@ package com.nameless.nameless_game.model;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Level describes a level in the game.
@@ -16,6 +17,8 @@ public class Level {
 	private Border border;
 
 	private ArrayList<Entity> entities;
+	// The types needed to progress
+	private Array<Entity> keyTypes;
 
 	/**
 	 * Constructs a level with a player and a physics world.
@@ -36,6 +39,16 @@ public class Level {
 		border = new Border(width, height, world);
 
 		entities = new ArrayList<Entity>();
+		
+		keyTypes =  new Array<Entity>(5);
+	}
+
+	public Array<Entity> getKeyTypes() {
+		return keyTypes;
+	}
+
+	public void setKeyTypes(Array<Entity> keyTypes) {
+		this.keyTypes = keyTypes;
 	}
 
 	public World getWorld() {
@@ -69,9 +82,18 @@ public class Level {
 	public boolean addEntity(Entity entity) {
 		if (entity.getBody().getWorld().equals(world)) {
 			entities.add(entity);
+			getTypes();
 			return true;
 		} else {
 			return false;
+		}
+	}
+	
+	private void getTypes() {
+		for (Entity entity : entities) {
+			if (!keyTypes.contains(entity, true)) {
+				keyTypes.add(entity);
+			}
 		}
 	}
 }
