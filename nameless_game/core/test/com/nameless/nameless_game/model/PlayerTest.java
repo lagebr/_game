2ab2f5f69e7ce1685b842bdca7818132ed3279d7
lagebr <lagebr@kth.se>
2016-05-14 17:14:13 +0200

    Added files for some test cases.

---
+++ nameless_game/core/test/com/nameless/nameless_game/model/PlayerTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.nameless.nameless_game.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lagebr
+ *
+ */
+public class PlayerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
