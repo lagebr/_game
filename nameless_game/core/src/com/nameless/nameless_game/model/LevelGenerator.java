6078dd1bf7642973248d87e34b3316b0345aa108
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-14 13:39:14 +0200

    Changed name from world to level generator.
    This is due to the generator being responsible for creating levels.

--- nameless_game/core/src/com/nameless/nameless_game/model/LevelGenerator.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/LevelGenerator.java
@@ -1,23 +1,19 @@
-/**
- * 
- */
 package com.nameless.nameless_game.model;
 
 /**
  * A procedural level generator to add seemingly infinite variety. Included in
- * this generating is all the hostie spawn & type, player spawn, fuel cells,
+ * this generating is all the hostile spawn & type, player spawn, fuel cells,
  * key-hole for level progression, static elements etc.
  * 
  * @author Isaac Arvestad, Henrik Lagebrand
  *
  */
-public class WorldGenerator {
+public class LevelGenerator {
 
 	/**
 	 * 
 	 */
-	public WorldGenerator() {
+	public LevelGenerator() {
 		// TODO Auto-generated constructor stub
 	}
-
 }
