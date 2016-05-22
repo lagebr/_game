6c7757b021144d30a4270fdcd7cce8c433822478
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-22 13:05:57 +0200

    Render text prompt to screen.

--- nameless_game/core/src/com/nameless/nameless_game/controller/MenuController.java
+++ nameless_game/core/src/com/nameless/nameless_game/controller/MenuController.java
@@ -3,7 +3,10 @@ package com.nameless.nameless_game.controller;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * MenuController is responsible for updating the main menu and drawing it to
@@ -16,14 +19,20 @@ public class MenuController implements Screen {
 
 	private NamelessGame game;
 
+	private SpriteBatch batch;
+	private BitmapFont font;
+
 	/**
 	 * Creates a menu controller with a reference to NamelessGame. The reference
 	 * is used when game should start.
 	 * 
-	 * @param game 
+	 * @param game
 	 */
 	public MenuController(NamelessGame game) {
 		this.game = game;
+
+		batch = new SpriteBatch();
+		font = new BitmapFont();
 	}
 
 	@Override
@@ -37,8 +46,13 @@ public class MenuController implements Screen {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		if (Gdx.app.getInput().isKeyPressed(Input.Keys.SPACE)) {
+		batch.begin();
+		font.setColor(Color.BLACK);
+		font.draw(batch, "Press <SPACE> to begin", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		batch.end();
 
+		if (Gdx.app.getInput().isKeyPressed(Input.Keys.SPACE)) {
+			game.startGame();
 		}
 	}
 
