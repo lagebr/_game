78e2e07e1b6483d383dca454d7798547c3ce2d60
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-22 12:49:50 +0200

    Created a class which is taking over NamelessGame responsibilities.

---
+++ nameless_game/core/src/com/nameless/nameless_game/controller/GameController.java
@@ -0,0 +1,56 @@
+package com.nameless.nameless_game.controller;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * GameController is responsible for updating the game and drawing it to screen
+ * using Renderer/ScreenRenderer.
+ * 
+ * @author Isaac Arvestad, Henrik Lagebrand
+ * @version 2016-05-22
+ */
+public class GameController implements Screen {
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
