5c53a867b026334e430687d015e67a2ac8486cdb
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-23 18:31:29 +0200

    Removed import.

--- nameless_game/core/src/com/nameless/nameless_game/model/LevelGenerator.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/LevelGenerator.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
-import com.nameless.nameless_game.render.ScreenGameRenderer;
 
 /**
  * A procedural level generator to add seemingly infinite variety. Included in
