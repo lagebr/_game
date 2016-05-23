0cee1c231f082e56c260b492bc5c357c5c12df39
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-23 16:00:06 +0200

    Player losing now results in game over screen being shown.

--- nameless_game/core/src/com/nameless/nameless_game/controller/GameController.java
+++ nameless_game/core/src/com/nameless/nameless_game/controller/GameController.java
@@ -128,7 +128,7 @@ public class GameController implements Screen {
 				if (contact.getFixtureA().getFilterData().categoryBits == Entity.PLAYER_ENTITY) {
 					if (contact.getFixtureB().getUserData() instanceof Hostile) {
 						if (level.getPlayer().isBoosting() == false) {
-							game.startMainMenu();
+							game.startGameOver();
 						} else {
 							Hostile hostileB = (Hostile) contact.getFixtureB().getUserData();
 							hostileB.setFlaggedForDeletion(true);
@@ -138,7 +138,7 @@ public class GameController implements Screen {
 				} else if (contact.getFixtureB().getFilterData().categoryBits == Entity.PLAYER_ENTITY) {
 					if (contact.getFixtureA().getUserData() instanceof Hostile) {
 						if (level.getPlayer().isBoosting() == false) {
-							game.startMainMenu();
+							game.startGameOver();
 						} else {
 							Hostile hostileA = (Hostile) contact.getFixtureA().getUserData();
 							hostileA.setFlaggedForDeletion(true);
