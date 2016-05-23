5fd1a81db60e08ce6a02e23e2573c9c6647d593f
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-23 15:24:55 +0200

    Implemented a menu renderer.

--- nameless_game/core/src/com/nameless/nameless_game/render/MainMenuRenderer.java
+++ nameless_game/core/src/com/nameless/nameless_game/render/MainMenuRenderer.java
@@ -1,5 +1,11 @@
 package com.nameless.nameless_game.render;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * MainMenuRenderer is responsible for rendering the main menu.
  * 
@@ -8,17 +14,28 @@ package com.nameless.nameless_game.render;
  */
 public class MainMenuRenderer {
 
+	private SpriteBatch batch;
+	private BitmapFont font;
+	
 	/**
 	 * Creates a new MainMenuRenderer.
 	 */
 	public MainMenuRenderer() {
-		
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+
 	}
 	
 	/**
 	 * Renders the main menu.
 	 */
 	public void render() {
-		
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		font.setColor(Color.WHITE);
+		font.draw(batch, "Press <P> to begin", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		batch.end();
 	}
 }
