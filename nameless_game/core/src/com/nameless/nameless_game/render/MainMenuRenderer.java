a2b41b5e61839d60151f83183143e0baf080db48
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-23 15:13:54 +0200

    Created a renderer for the main menu.

---
+++ nameless_game/core/src/com/nameless/nameless_game/render/MainMenuRenderer.java
@@ -0,0 +1,24 @@
+package com.nameless.nameless_game.render;
+
+/**
+ * MainMenuRenderer is responsible for rendering the main menu.
+ * 
+ * @author Isaac Arvestad, Henrik Lagebrand
+ * @version 2016-05-23
+ */
+public class MainMenuRenderer {
+
+	/**
+	 * Creates a new MainMenuRenderer.
+	 */
+	public MainMenuRenderer() {
+		
+	}
+	
+	/**
+	 * Renders the main menu.
+	 */
+	public void render() {
+		
+	}
+}
