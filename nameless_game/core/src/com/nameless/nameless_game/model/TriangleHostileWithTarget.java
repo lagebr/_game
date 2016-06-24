32180357b6197a0cd2891bd3d723aefd297f4e52
lagebr <lagebr@kth.se>
2016-06-25 00:57:18 +0200

    Removed init-method, moved to helper.

--- nameless_game/core/src/com/nameless/nameless_game/model/TriangleHostileWithTarget.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/TriangleHostileWithTarget.java
@@ -2,12 +2,6 @@ package com.nameless.nameless_game.model;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.nameless.nameless_game.render.ScreenGameRenderer;
 
@@ -27,8 +21,8 @@ public class TriangleHostileWithTarget extends Hostile {
 
 		type = HostileType.TRIANGLE;
 
-		body = createDynamicTriangleBody(ScreenGameRenderer.pixelToMeter(x),
-				ScreenGameRenderer.pixelToMeter(y), world);
+		body = PhysicsHelper.createDynamicTriangleBody(ScreenGameRenderer.pixelToMeter(x),
+				ScreenGameRenderer.pixelToMeter(y), world, this);
 
 		updateSprite();
 	}
@@ -44,44 +38,6 @@ public class TriangleHostileWithTarget extends Hostile {
 		float y = (float) (-dy / Math.abs(dy) * 0.05);
 
 		body.applyLinearImpulse(new Vector2(x, y), body.getWorldCenter(), true);
-	}
-
-	/**
-	 * Creates a dynamic triangle body.
-	 * 
-	 * @param x
-	 *            Center x-coordinate of triangle in pixels.
-	 * @param y
-	 *            Center y-coordinate of triangle in pixels.
-	 * @param world
-	 *            The physics world to add it to.
-	 * @return The create physics body.
-	 */
-	public Body createDynamicTriangleBody(float x, float y, World world) {
-		BodyDef bodyDef = PhysicsHelper.createBodyDef(x, y,
-				BodyType.DynamicBody, false);
-		Body physicsBody = world.createBody(bodyDef);
-		physicsBody.setFixedRotation(true);
-
-		Vector2 p1 = new Vector2(0, 0);
-		Vector2 p2 = new Vector2(ScreenGameRenderer.pixelToMeter(70), 0);
-		Vector2 p3 = new Vector2(ScreenGameRenderer.pixelToMeter(35),
-				ScreenGameRenderer.pixelToMeter(60));
-
-		PolygonShape triangle = new PolygonShape();
-		triangle.set(new Vector2[]{p1, p2, p3});
-
-		FixtureDef fixtureDef = PhysicsHelper.createFixture(triangle, 10.0f);
-		// Collision masks
-		fixtureDef.filter.categoryBits = Entity.NPC_ENTITY;
-		fixtureDef.filter.maskBits = Entity.PLAYER_ENTITY | Entity.NPC_ENTITY;
-
-		Fixture fixture = physicsBody.createFixture(fixtureDef);
-		fixture.setUserData(this);
-
-		triangle.dispose(); // LibGDX
-
-		return physicsBody;
 	}
 
 	/**
