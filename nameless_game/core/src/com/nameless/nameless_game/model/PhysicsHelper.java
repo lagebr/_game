3397917cd49c08e1352ba4392d1f4037fab3a59e
lagebr <lagebr@kth.se>
2016-06-25 00:56:47 +0200

    Moved constructor to helper.

--- nameless_game/core/src/com/nameless/nameless_game/model/PhysicsHelper.java
+++ nameless_game/core/src/com/nameless/nameless_game/model/PhysicsHelper.java
@@ -1,8 +1,10 @@
 package com.nameless.nameless_game.model;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.nameless.nameless_game.render.ScreenGameRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
@@ -231,6 +233,44 @@ public class PhysicsHelper {
 		fixture.setUserData(objRef);
 
 		rectangle.dispose(); // LibGDX
+
+		return physicsBody;
+	}
+	
+	/**
+	 * Creates a dynamic triangle body.
+	 * 
+	 * @param x
+	 *            Center x-coordinate of triangle in pixels.
+	 * @param y
+	 *            Center y-coordinate of triangle in pixels.
+	 * @param world
+	 *            The physics world to add it to.
+	 * @return The create physics body.
+	 */
+	public static Body createDynamicTriangleBody(float x, float y, World world, Hostile self) {
+		BodyDef bodyDef = PhysicsHelper.createBodyDef(x, y,
+				BodyType.DynamicBody, false);
+		Body physicsBody = world.createBody(bodyDef);
+		physicsBody.setFixedRotation(true);
+
+		Vector2 p1 = new Vector2(0, 0);
+		Vector2 p2 = new Vector2(ScreenGameRenderer.pixelToMeter(70), 0);
+		Vector2 p3 = new Vector2(ScreenGameRenderer.pixelToMeter(35),
+				ScreenGameRenderer.pixelToMeter(60));
+
+		PolygonShape triangle = new PolygonShape();
+		triangle.set(new Vector2[]{p1, p2, p3});
+
+		FixtureDef fixtureDef = PhysicsHelper.createFixture(triangle, 10.0f);
+		// Collision masks
+		fixtureDef.filter.categoryBits = Entity.NPC_ENTITY;
+		fixtureDef.filter.maskBits = Entity.PLAYER_ENTITY | Entity.NPC_ENTITY;
+
+		Fixture fixture = physicsBody.createFixture(fixtureDef);
+		fixture.setUserData(self);
+
+		triangle.dispose(); // LibGDX
 
 		return physicsBody;
 	}
